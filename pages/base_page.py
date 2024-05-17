6b91d42b1061a17a87bc10c560edd0c071c3cf0d
Nariman Mirzakhanov <mnariman05@gmail.com>
2024-05-17 01:08:53 -0700

    done changes in base_page

--- pages/base_page.py
+++ pages/base_page.py
@@ -12,7 +12,7 @@ class BasePage:
 
     def __init__(self, driver):
         self.driver = driver
-
+    #this is the click method
     def click(self, locator):
         if str(locator).endswith("_XPATH"):
             self.driver.find_element(AppiumBy.XPATH, (config_reader.read_config("locators", locator))).click()
